95f33c81ea92747da9984edf35b6807614d089f7
Lautaro Laserna <lasernalauta@gmail.com>
2023-02-07 02:24:27 -0300

    Cambie todo

--- src/pages/Home/components/Welcome/Welcome.tsx
+++ src/pages/Home/components/Welcome/Welcome.tsx
@@ -1,40 +1,41 @@
-import { useEffect, useState, useContext } from 'react'
-import { Button, SocialIcon } from '@/components'
 import { ThemeContext } from '@/App'
-import ig from '@/assets/ig.png'
 import git from '@/assets/git.png'
+import ig from '@/assets/ig.png'
 import linkedin from '@/assets/linkedin.png'
+import { Button, SocialIcon } from '@/components'
+import { useContext } from 'react'
 import './Welcome.css'
 
 export interface WelcomeInterface {}
 
 const Welcome: React.FC<WelcomeInterface> = () => {
-/*   const letters = ['L', 'a', 'u', 't', 'i', '.']
-  const [name, setName] = useState('')
-  const [pos, setPos] = useState(0) */
-  
-  /*   useEffect(() => {
-    setTimeout(() => {
-      if (pos < letters.length) {
-        setName(name => name.concat(letters[pos]))
-        setPos(p => p + 1)
-      }
-    }, 120)
-  }, [pos]) */
-  
   const { darkMode, toggleDarkMode } = useContext(ThemeContext)
-  
+
   return (
     <section className={`Welcome ${darkMode ? 'darkmode' : ''}`} id="home">
-      <div className="welcome-hero">
-        <h1 className="title">Lautaro Laserna</h1>
-        <h4 className="desc">Software engineer & Full stack developer</h4>
+      <div className="container-left">
+        <h1 className="title">Welcome</h1>
+        <h1 className="title clear">Welcome</h1>
+        <h1 className="title">Welcome</h1>
+        <h1 className="title clear">Welcome</h1>
+        <h1 className="title">Welcome</h1>
+        <h1 className="title clear">Welcome</h1>
+        <h1 className="title">Welcome</h1>
       </div>
-      <Button darkMode={darkMode} content="Contact me" />
-      <div className="social-container">
-        <SocialIcon icon={linkedin} darkMode={darkMode} url={'https://www.linkedin.com/in/lautaro-laserna/'} />
-        <SocialIcon icon={git} darkMode={darkMode} url={'https://github.com/lautalaserna'} />
-        <SocialIcon icon={ig} darkMode={darkMode} url={'https://www.instagram.com/lautalaserna/'} />
+      <div className="container-right">
+        <div className="name-container">
+          <h1 className="name">I'm Lautaro Laserna</h1>
+          <h4 className="desc">Software engineer & Full stack developer</h4>
+        </div>
+        <div className="btn-container">
+          <Button darkMode={darkMode} content="See my Work" />
+          <Button darkMode={darkMode} content="Let's Talk" />
+        </div>
+        <div className="social-container">
+          <SocialIcon icon={linkedin} darkMode={darkMode} url={'https://www.linkedin.com/in/lautaro-laserna/'} />
+          <SocialIcon icon={git} darkMode={darkMode} url={'https://github.com/lautalaserna'} />
+          <SocialIcon icon={ig} darkMode={darkMode} url={'https://www.instagram.com/lautalaserna/'} />
+        </div>
       </div>
     </section>
   )
